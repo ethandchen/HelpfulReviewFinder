f4a2c5bfb89a15b09e233d252c4c50d63d4f1e51
ethandc <ethandc@umich.edu>
2023-04-09 17:22:57 -0400

    implemented glove embeddings

--- project_glove.py
+++ project_glove.py
@@ -2,7 +2,35 @@ import numpy as np
 # import pandas as pd
 import csv,re,string
 from sklearn import svm
+from sklearn.decomposition import PCA
 import glob,os
+
+def preprocess_glove_embeddings(embedding_dim):
+    word_to_index = {}    #0: padding, 1: UNKA
+    glove_embeddings = []
+    #add first two entries of random numbers for PAD and UNKA
+    zero_embedding = []
+    for a in range(embedding_dim):
+        zero_embedding.append(0)
+    glove_embeddings.append(zero_embedding)
+    rand_embedding = []
+    for a in range(embedding_dim):
+        rand_embedding.append(np.random.normal())
+    glove_embeddings.append(rand_embedding)
+
+    glove_file = "glove.6B.50d.txt"
+    glove_path = os.path.join(os.getcwd(), "glove.6B", glove_file)
+    with open(glove_path, "r", encoding="utf-8") as reader:
+        index = 2
+        for line in reader:
+            glove_line = line
+            glove_list = glove_line.split()
+            word = glove_list[0]
+            word_to_index[word] = index
+            embedding = [float(e) for e in glove_list[1:]]
+            glove_embeddings.append(embedding)
+            index += 1
+    return word_to_index, glove_embeddings
 
 # preprocess each review
 def preprocessing(text):
@@ -19,18 +47,27 @@ def preprocessing(text):
     return result
 
 
-def generate_feature_matrix(list_of_tokens, vocab):
+def generate_feature_matrix(list_of_tokens, word_to_index, glove_embeddings):
     number_of_reviews = len(list_of_tokens)
-    number_of_words = len(vocab)
-    feature_matrix = np.zeros((number_of_reviews, number_of_words))
-    for i, reviews in enumerate(list_of_tokens):
-        for token in reviews:
-            try:
-                col = vocab[token]
-                feature_matrix[i][col] += 1
-            except: print("somethings wrong")
+    max_dim = 10000
+    feature_matrix = np.zeros((number_of_reviews, max_dim))
+    for i, review in enumerate(list_of_tokens):
+        counter = 0
+        for token in review:
+            if token in word_to_index:
+                ind = word_to_index[token]
+                embedding = glove_embeddings[ind]
+                for k, num in enumerate(embedding):
+                    feature_matrix[i][counter] = embedding[k]
+                    counter += 1
+            if counter > max_dim-51:
+                break
+    pca_model = PCA(n_components=600)
+    pca_model.fit(feature_matrix)
+    pca_model.fit(feature_matrix)
+    pca_mat = pca_model.transform(feature_matrix)
 
-    return feature_matrix
+    return pca_mat
 
 def train(features, labels):
     model = svm.LinearSVC(penalty="l2", loss="hinge", dual=True, C=1, random_state=486, max_iter=10000)
@@ -48,10 +85,12 @@ def main():
     X_labels = []     #1d list
     Y_labels = []
 
+    # preprocess glove embeddings
+    word_to_index, glove_embeddings = preprocess_glove_embeddings(50)
+
     # get list of training and testing files
     training_files = glob.glob(os.path.join("training/", "*.csv"))
     testing_files = glob.glob(os.path.join("testing/", "*.csv"))
-    vocab = {}
 
     # preprocess training data
     for training_file in training_files:
@@ -59,8 +98,6 @@ def main():
             reader = csv.reader(csvfile)
             for row in reader:
                 my_string = preprocessing(row[0])
-                for s in my_string:
-                    if s not in vocab: vocab[s] = len(vocab)
                 X_reviews.append(my_string)
                 X_labels.append(row[2])
 
@@ -70,23 +107,19 @@ def main():
             reader = csv.reader(csvfile)
             for row in reader:
                 my_string = preprocessing(row[0])
-                for s in my_string:
-                    if s not in vocab: vocab[s] = len(vocab)
                 Y_reviews.append(my_string)
                 Y_labels.append(row[2])
 
     X_labels = np.array(X_labels[1:])
     Y_labels = np.array(Y_labels[1:])
 
-    X_features = generate_feature_matrix(X_reviews[1:], vocab)
-    Y_features = generate_feature_matrix(Y_reviews[1:], vocab)
+    X_features = generate_feature_matrix(X_reviews[1:], word_to_index, glove_embeddings)
+    Y_features = generate_feature_matrix(Y_reviews[1:], word_to_index, glove_embeddings)
     # train classifier
     classifier = train(X_features, X_labels)
     # test classifier
     test_predict = predict(classifier, Y_features)
     # evaluate
-    # compare Y_labels with test_predict
-    # print(len(Y_labels), len(test_predict))
     correct = 0
     for i in range(0, len(Y_labels)):
         print(test_predict[i])
